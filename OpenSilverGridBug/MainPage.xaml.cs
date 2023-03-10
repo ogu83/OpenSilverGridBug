9b8e5815cb0a91f42c02be547886a78e7ba7eb19
oguzkoroglu <oguzkoroglu@gmail.com>
2023-03-10 12:14:37 +0300

    Add project files.

---
+++ OpenSilverGridBug/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverGridBug
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //this.mainGrid.Children.Add(new Page1());
+            for (int i = 0; i < 20; i++)
+            {
+                tcWorkspaceTabs2.Items.Add(new TabItem { Header = $"Tab Item {i}" });
+            }
+        }
+    }
+}
